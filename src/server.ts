0e155157aa61240fd53dab2a3da205e95ac4e350
Caique Reis <caiquereiss@outlook.com>
2023-02-08 19:29:46 -0300

    implement zod

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import fastify from 'fastify'
+
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -12,7 +14,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
